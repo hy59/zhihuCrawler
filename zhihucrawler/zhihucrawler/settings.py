4133bf31dbbc323e73f0f957b501c441e4746431
zealerHuang <937162905@qq.com>
2017-04-12 23:23:42 +0800

    添加存储方式，使用mongodb存储

--- zhihucrawler/zhihucrawler/settings.py
+++ zhihucrawler/zhihucrawler/settings.py
@@ -64,9 +64,10 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'zhihucrawler.pipelines.ZhihucrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'zhihucrawler.pipelines.MongoPipeline': 300,
+}
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +89,6 @@ DEFAULT_REQUEST_HEADERS = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI = 'localhost'
+MONGO_DATABASE = 'zhihu'
