d09cea69e8675c20e9770b2bc784e5afe600540f
zealerHuang <937162905@qq.com>
2017-04-12 13:16:14 +0800

    完善爬虫程序

--- zhihucrawler/zhihucrawler/spiders/zhihu.py
+++ zhihucrawler/zhihucrawler/spiders/zhihu.py
@@ -3,18 +3,24 @@
 import json
 
 from scrapy import Spider, Request
-from zhihuuser.items import UserItem
+from zhihucrawler.items import UserItem
 
 
 class ZhihuSpider(Spider):
     name = "zhihu"
     allowed_domains = ["www.zhihu.com"]
     user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    # 用户关注的人
     follows_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&offset={offset}&limit={limit}'
+    # 关注用户的人
     followers_url = 'https://www.zhihu.com/api/v4/members/{user}/followers?include={include}&offset={offset}&limit={limit}'
-    start_user = 'tianshansoft'
+    # 程序开始用户，这里选择轮子哥
+    start_user = 'excited-vczh'
+    # 获取用户的信息
     user_query = 'locations,employments,gender,educations,business,voteup_count,thanked_Count,follower_count,following_count,cover_url,following_topic_count,following_question_count,following_favlists_count,following_columns_count,answer_count,articles_count,pins_count,question_count,commercial_question_count,favorite_count,favorited_count,logs_count,marked_answers_count,marked_answers_text,message_thread_token,account_status,is_active,is_force_renamed,is_bind_sina,sina_weibo_url,sina_weibo_name,show_sina_weibo,is_blocking,is_blocked,is_following,is_followed,mutual_followees_count,vote_to_count,vote_from_count,thank_to_count,thank_from_count,thanked_count,description,hosted_live_count,participated_live_count,allow_message,industry_category,org_name,org_homepage,badge[?(type=best_answerer)].topics'
+    # 获取用户关注的人的信息
     follows_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+    # 获取关注用户的人的信息
     followers_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
 
     def start_requests(self):
@@ -24,6 +30,7 @@ class ZhihuSpider(Spider):
         yield Request(self.followers_url.format(user=self.start_user, include=self.followers_query, limit=20, offset=0),
                       self.parse_followers)
 
+    # 解析用户的信息
     def parse_user(self, response):
         result = json.loads(response.text)
         item = UserItem()
@@ -41,6 +48,7 @@ class ZhihuSpider(Spider):
             self.followers_url.format(user=result.get('url_token'), include=self.followers_query, limit=20, offset=0),
             self.parse_followers)
 
+    # 解析用户关注的人的信息
     def parse_follows(self, response):
         results = json.loads(response.text)
 
@@ -54,6 +62,7 @@ class ZhihuSpider(Spider):
             yield Request(next_page,
                           self.parse_follows)
 
+    # 解析关注用户的人的信息
     def parse_followers(self, response):
         results = json.loads(response.text)
 
